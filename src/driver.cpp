80fcf769e0eee9e7983eeb00a97d07bd69b22354
Gaku Takasawa <takasawa.gaku.or@tut.jp>
2022-01-25 13:28:27 +0900

    Fix select theta from devlist  & Add param thetaIndex

--- src/driver.cpp
+++ src/driver.cpp
@@ -21,12 +21,14 @@ void thetaVDriver::getparam()
 {
   m_param_useNVdec    = false;
   m_param_use4K       = false;
+  m_param_thetaIndex  = 0;
   m_param_rawOn       = false;
   m_param_compressOn  = false;
   m_param_pngLevel    = 3;
   // get param
   ros::param::get("~nvdec",                m_param_useNVdec   );
   ros::param::get("~use4k",                m_param_use4K      );
+  ros::param::get("~m_param_thetaIndex",   m_param_thetaIndex );
   ros::param::get("~image/raw/on",         m_param_rawOn      );
   ros::param::get("~image/compress/on",    m_param_compressOn );
   ros::param::get("~image/compress/level", m_param_pngLevel   );
